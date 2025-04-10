b9000f59a44e45ef47f417091c644c8bed99dd51
alexandermoroz01 <alexandermoroz01@gmail.com>
2025-04-10 19:19:02 +0300

    ['html', {open: 'always'}],

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,13 +25,12 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: 1,//process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  // [
-    // ['html'],
-    // ['allure-playwright', {
-    //   outputFolder: 'allure-results',
-    // }]
-  // ],
+  reporter:[
+    ['html', {open: 'always'}],
+    ['allure-playwright', {
+      outputFolder: 'allure-results',
+    }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
